f8134611390056c296a15126651306f7840212c5
Mia Hall <mia@mhall.ca>
2023-06-21 15:10:48 -0400

    Basic support for new GH layout

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const nav = '.Header-item nav';
-const menuItemClassNames = 'js-selected-navigation-item Header-link mt-md-n3 mb-md-n3 py-2 py-md-3 mr-0 mr-md-3 border-top border-md-top-0 border-white-fade';
+const nav = '[role="navigation"] > [role="list"]';
+const menuItemClassNames = 'AppHeader-context-item';//'js-selected-navigation-item Header-link mt-md-n3 mb-md-n3 py-2 py-md-3 mr-0 mr-md-3 border-top border-md-top-0 border-white-fade';
 const shortcuts = ShortcutsStorage.getShortcuts();
 const menuDialog = new GHDialog();
 
@@ -23,8 +23,8 @@ function addShortcut(title, path) {
     return shortcut;
 }
 function createShortcutLink(shortcut) {
-    const span = document.createElement('span');
-    span.className = 'ghsh-container';
+    const span = document.createElement('li');
+    span.style.position = 'relative';
     span.innerHTML = `
         <a href="${shortcut.path}" class="${menuItemClassNames}">${shortcut.title}</a>
         <button class="ghsh-remove btn-link Link--muted">${ShortcutsIcons.remove}</button>
@@ -37,13 +37,18 @@ function createShortcutLink(shortcut) {
     return span;
 }
 function createShortcutsNav() {
-    shortcutsNav = document.createElement('nav');
-    shortcutsNav.className = `shortcuts-menu ${navmenu.className}`;
+    shortcutsNav = document.createDocumentFragment();
     shortcutsNav.append(
         ...shortcuts.map(shortcut => createShortcutLink(shortcut))
     );
 
     return shortcutsNav;
+}
+function createSeparator() {
+    const span = document.createElement('li');
+    span.innerHTML = '<span style="width: 1px; height: var(--base-size-20, 20px); background: var(--color-border-default); margin: 0 calc((var(--control-medium-size, 32px) - var(--base-size-20, 20px))/2);" />';
+    
+    return span;
 }
 function createAddButton() {
     const button = document.createElement('button');
@@ -67,7 +72,8 @@ function createAddButton() {
 const navmenu = document.querySelector(nav);
 
 if (navmenu) {
-    navmenu.after(
+    navmenu.append(
+        createSeparator(),
         createShortcutsNav(),
         createAddButton()
     );
